3bc27327f9ad9ef30bca5038f9b11553bf86d386
REBOUH, Nassim <n.rebouh@insta.fr>
2017-02-26 16:02:59 +0100

    add annotations to build boot jar

--- src/main/java/com/rebouhna/MainApp.java
+++ src/main/java/com/rebouhna/MainApp.java
@@ -2,8 +2,12 @@ package com.rebouhna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.ContextConfiguration;
 
 @EnableAutoConfiguration
+@SpringBootApplication
+@ContextConfiguration
 public class MainApp {
 	public static void main(String[] args) throws Exception {
         SpringApplication.run(MainApp.class, args);
